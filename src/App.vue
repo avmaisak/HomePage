654ec744b267eea1c2de446a2229e198ab48f6b3
Anton Maisak <am@rus-bit.com>
2018-07-21 10:30:24 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style lang="scss" src="@/module/Matrioshka/src/scss/matrioshka.complete.scss"></style>
+<style lang="scss" src="@/module/Matrioshka/dist/icons/css/matrioshkaIcons.min.css"></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="cont">
+        <div class="rw">
+          <div class="cl w-12">
+            <div class="txt-c p-t-2">
+              <h1 class=" txt-300 txt-sz-4">{{name}}</h1>
+              <p class="m-b-2 txt-sz-1-6 clr-grey-600 txt-200">{{desc}}</p>
+              <div>
+                <img src="@/assets/ava.jpg" class="ava m-b-2" width=180 :alt="name" />
+              </div>
+
+              <div class="clr-grey-700" id="links">
+                <a :href="github" class="txt-sz-2-5 m-1"><i class="mi-github"></i></a>
+                <a :href="email"  class="txt-sz-2-5 m-1"><i class="mi-envelope-out"></i></a>
+                <a :href="inst"  class="txt-sz-2-5 m-1"><i class="mi-instagram"></i></a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ScrollReveal from 'scrollreveal'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      name: 'Anton Maisak',
+      desc: 'Software develop, photo',
+      github: 'https://github.com/avmaisak',
+      email: 'mailto:anton@maisak.ru',
+      inst: 'https://www.instagram.com/anton.maisak/'
+    }
+  },
+  mounted () {
+    window.sr = ScrollReveal()
+    window.sr.reveal('h1')
+    window.sr.reveal('.ava', 200)
+    window.sr.reveal('#links a', 300)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+img.ava {
+  border-radius: 50%;
+}
+</style>
